f21798d7a05fd0c3d189de89cd67ae18644a805f
Somanshu Rath <114831855+somanshurath@users.noreply.github.com>
2023-04-03 15:02:46 +0530

    Day 03 update.

---
+++ pizza-bill.py
@@ -0,0 +1,30 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ").lower()
+add_pepperoni = input("Do you want pepperoni? Y or N ").lower()
+extra_cheese = input("Do you want extra cheese? Y or N ").lower()
+
+bill = 0
+
+if size == "s":
+    bill += 15
+elif size == "m":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "y" and size == "s":
+    bill += 2
+if add_pepperoni == "y" and size == "m":
+    bill += 3
+
+if extra_cheese == "y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
+
+
+
+
+
+
+
